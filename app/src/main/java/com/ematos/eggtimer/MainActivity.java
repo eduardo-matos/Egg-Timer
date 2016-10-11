b94569988a93d50334d99fb34ec97b0d6e113e13
Eduardo Matos <eduardo.matos.silva@gmail.com>
2016-10-11 08:08:01 -0300

    timer follows seekbar

--- app/src/main/java/com/ematos/eggtimer/MainActivity.java
+++ app/src/main/java/com/ematos/eggtimer/MainActivity.java
@@ -2,6 +2,8 @@ package com.ematos.eggtimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timeSeeker = (SeekBar) findViewById(R.id.timeSeeker);
+        timeSeeker.setMax(600);
+        timeSeeker.setProgress(30);
+
+        final TextView timer = (TextView) findViewById(R.id.timer);
+
+        timeSeeker.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int minutes = progress / 60;
+                int seconds = progress - (minutes * 60);
+
+                timer.setText(String.format("%02d:%02d", minutes, seconds));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
